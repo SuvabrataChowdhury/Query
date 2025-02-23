d431073835a712eef7e7f9054ed9b7d6c8ba2860
I578071 <suvabrata.chowdhury@sap.com>
2025-02-23 19:56:03 +0530

    setup base

--- commands/from.sh
+++ commands/from.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "This is from"
+function cmd::from(){
+	strm_src=$1
+	
+	if [[ -f $strm_src ]]; then
+		cat $strm_src
+	elif [[ -d $strm_src ]]; then
+		ls $strm_src
+	else
+		echo "Invalid Stream source given"
+		return 1
+	fi
+}
+
+
+read -p "Enter source: " strm_src
+cmd::from $strm_src
+
--- commands/select.sh
+++ commands/select.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "This is select"
+#TODO use a filter to filter through the given stream
+function cmd::select() {
+	stream=$1
+	
+	echo $stream
+}
+
+#TODO for now take input from user later to be supplied by supervising command
+read -p "Enter Stream: " stream
+
+cmd::select "$stream"
