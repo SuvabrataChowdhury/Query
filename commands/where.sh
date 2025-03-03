e12bdbddf3ef383324e643c5b06d0d53458337ae
I578071 <suvabrata.chowdhury@sap.com>
2025-03-04 00:23:30 +0530

    Added base code. Need to test and refactor

--- commands/where.sh
+++ commands/where.sh
@@ -1,10 +1,76 @@
-#!/bin/bash
+#!/bin/zsh
 
-#
-#
+#TODO: evaluate any/all expressions
+
+stream=("Hello World" " abc")
 
 function cmd::where() {
-	echo "Inside command"
+	local exprss=$1
+	
+	
+	if [[ $(is_valid_expression "$exprss") ]]
+	then
+		result=()
+		
+		for item in ${stream[@]}
+		do
+			if [[ $(evaluate_expr "$item" "$exprss") ]]
+			then
+				result+=("$item")
+			fi
+		done
+	fi
+}
+
+#TODO make it work for compound statements
+function is_valid_expression() {
+	local exprss=$1
+	local regex="^(like|length) "
+
+	if [[ "$exprss" =~ "$regex" ]]
+	then
+		local exprs_arr=("$exprss")
+		if [[ ${#exprs_arr[@]} != 2 ]]
+		then
+			return 0
+		fi
+		
+		if [[ ${exprs_arr[0]} == "length" ]] && [[ ${exprs_arr[1]} =~ "[0-9]+" ]]
+		then
+			return 0
+		fi
+	fi
+	
+	return 1
+}
+
+#TODO make it handle more function
+function evaluate_expr() {
+	local tst_item=$1
+	local exprss=$2
+	
+	local exprs_arr=("$exprss")
+	
+	case ${exprs_arr[0]} in
+		"length")
+			if [[ ${#tst_item[@]} == ${exprs_arr[1]} ]]
+			then
+				return 0
+			fi
+			;;
+		"like")
+		
+			if [[ $tst_item =~ "${exprs_arr[1]}" ]]
+			then
+				return 0
+			fi
+			;;
+		
+		*)
+			;;
+	esac
+	
+	return 1
 }
 
 echo "This is where"
