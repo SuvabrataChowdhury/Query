20eca1c1ac66c5c0a78a7fc8a2ea3a354ebc0e81
I578071 <suvabrata.chowdhury@sap.com>
2025-03-09 12:04:20 +0530

    Implemented Where (base)

--- commands/where.sh
+++ commands/where.sh
@@ -1,76 +1,42 @@
-#!/bin/zsh
+#!/bin/bash
 
 #TODO: evaluate any/all expressions
-
 stream=("Hello World" " abc")
+result=()
 
-function cmd::where() {
-	local exprss=$1
+QRY_HOME="/Users/I578071/MiniProjects/SystemsProgramming/Query"
+
+source $QRY_HOME/lib/expression.sh
+
+function cmd::where {
+	local exprss="$1"
 	
-	
-	if [[ $(is_valid_expression "$exprss") ]]
+	if is_valid_expression "$exprss"
 	then
+		echo "valid expression"
+		
 		result=()
 		
 		for item in ${stream[@]}
 		do
-			if [[ $(evaluate_expr "$item" "$exprss") ]]
+			echo "item: $item expression: $exprss"
+			
+			if evaluate_expression "$item" "$exprss"
 			then
+				echo "expression satisfied"
 				result+=("$item")
 			fi
 		done
 	fi
 }
 
-#TODO make it work for compound statements
-function is_valid_expression() {
-	local exprss=$1
-	local regex="^(like|length) "
-
-	if [[ "$exprss" =~ "$regex" ]]
-	then
-		local exprs_arr=("$exprss")
-		if [[ ${#exprs_arr[@]} != 2 ]]
-		then
-			return 0
-		fi
-		
-		if [[ ${exprs_arr[0]} == "length" ]] && [[ ${exprs_arr[1]} =~ "[0-9]+" ]]
-		then
-			return 0
-		fi
-	fi
-	
-	return 1
-}
-
-#TODO make it handle more function
-function evaluate_expr() {
-	local tst_item=$1
-	local exprss=$2
-	
-	local exprs_arr=("$exprss")
-	
-	case ${exprs_arr[0]} in
-		"length")
-			if [[ ${#tst_item[@]} == ${exprs_arr[1]} ]]
-			then
-				return 0
-			fi
-			;;
-		"like")
-		
-			if [[ $tst_item =~ "${exprs_arr[1]}" ]]
-			then
-				return 0
-			fi
-			;;
-		
-		*)
-			;;
-	esac
-	
-	return 1
-}
-
 echo "This is where"
+cmd::where "$1"
+
+unset QRY_HOME
+
+echo "filtered items:"
+for item in ${result[@]}
+do
+	echo "$item"
+done
