9cfff330b59ad592d00ba5b580bdea95e3ff4e7e
I578071 <suvabrata.chowdhury@sap.com>
2025-03-22 12:40:10 +0530

    Added the parser

--- commands/extractors/extract_args.sh
+++ commands/extractors/extract_args.sh
@@ -3,27 +3,73 @@
 QRY_HOME="/Users/I578071/MiniProjects/SystemsProgramming/Query"
 
 source $QRY_HOME/lib/string.sh
+source $QRY_HOME/lib/array.sh
 
 function extract_args {
-    local cmd=$1
-    local select_args_ref=$2
-    local from_args_ref=$3
-    local where_args_ref=$4
+    if [[ $# != 1 ]]
+    then
+        echo "Wrong number of arguments are given"
+        exit 1
+    fi
 
-    local select_pattern="select"
-    local from_pattern="from"
-    local where_pattern="where"
+    if declare -p select_args &>/dev/null && declare -p from_args &>/dev/null && declare -p where_args &>/dev/null
+    then
+        local cmd=$1
+        # local select_args_ref=$2
+        # local from_args_ref=$3
+        # local where_args_ref=$4
 
-    declare -a splt_cmd
-    split_string "$cmd" "\"" splt_cmd
+        local select_pattern="select"
+        local from_pattern="from"
+        local where_pattern="where"
 
-    local select_index=$(find_in_array "$select_pattern" "splt_cmd[@]")
-    local from_index=$(find_in_array "$from_pattern" "splt_cmd[@]")
-    local where_index=$(find_in_array "$where_pattern" "splt_cmd[@]")
+        # echo "Splitting string"
 
-    resource_file="$QRY_HOME/resources/pre_qry_args.lst"
+        declare -a splt_cmd
+        split_string "$cmd" "\"" splt_cmd
 
-    echo "${splt_cmd:$select_index:$(("$from_index"-"$select_index"))}" > $resource_file
-    echo "${splt_cmd:$fom_index:$(("$where_index"-"$from_index"))}" >> $resource_file
-    echo "${splt_cmd:$where_index}" >> $resource_file
+        local select_index=$(find_in_array "$select_pattern" "splt_cmd[@]")
+        local from_index=$(find_in_array "$from_pattern" "splt_cmd[@]")
+        local where_index=$(find_in_array "$where_pattern" "splt_cmd[@]")
+
+        if [[ $select_index -ge 0 ]] && [[ $from_index -ge 0 ]] && [[ $where_index -ge 0 ]]
+        then
+            # echo "$select_index $from_index $where_index"
+
+            local offset=$((from_index-select_index))
+
+            select_args="${splt_cmd[*]:$((select_index+1)):$((offset-1))}"
+            # echo "Select args: $select_args"
+
+            offset=$((where_index-from_index))
+            # echo "Offset: $offset"
+
+            from_args="${splt_cmd[*]:$((from_index+1)):$((offset-1))}"
+            # echo "From args: $from_args"
+
+            where_args="${splt_cmd[*]:$((where_index+1))}"
+            # echo "Where args: $where_args"
+
+
+            # resource_file="$QRY_HOME/resources/pre_qry_args.lst"
+
+            # echo "${splt_cmd:$select_index:$(("$from_index"-"$select_index"))}" > $resource_file
+            # echo "${splt_cmd:$fom_index:$(("$where_index"-"$from_index"))}" >> $resource_file
+            # echo "${splt_cmd:$where_index}" >> $resource_file
+        else
+            echo "Keyword Select or From or where is missing"
+            exit 1
+        fi
+    else
+        echo "Global variables are not set correctly"
+        exit 1
+    fi
 }
+
+#Driver code for testing
+
+# select_args=""
+# from_args=""
+# where_args=""
+
+# "$@"
---
+++ lib/array.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function find_in_array {
+    local pattern=$1
+    local arr_ref=$2
+
+    local index=0
+
+    for item in "${!arr_ref}"
+    do
+        # echo "$item"
+
+        if [[ $item =~ $pattern ]]
+        then
+            echo $index
+            return 0
+        fi
+
+        ((index++))
+    done
+
+    echo "-1"
+    return 1
+}
+
+# arr=(select "*" from file1.txt where "length 10")
+# find_in_array "select" arr[@]
+# find_in_array "from" arr[@]
+# find_in_array "where" arr[@]
