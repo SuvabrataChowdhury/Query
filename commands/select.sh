df01c61a6b95c26afb502a3c4140d4891676d9a1
I578071 <suvabrata.chowdhury@sap.com>
2025-04-16 00:27:12 +0530

    Somewhat working

--- commands/select.sh
+++ commands/select.sh
@@ -9,21 +9,100 @@ source $QRY_HOME/lib/string.sh
 #	TODO: cmd::select "$selection" "splitter" INPUT_STREAM
 
 function cmd::select {
+	# echo "Select Args: $@"
+
 	select_validate "$@"
 
-	local selection="$1"
-	local inp_stream_ref="$2"
+	local selection="$2"
+	local inp_stream_ref="$1"
+
+	# echo "$inp_stream_ref"
 	
 	local result=""
 
 	for token in "${!inp_stream_ref}"
 	do
 		# echo "$token"
-		result="$(substring_regex "$token" "$selection")"
+		# result'="$(substring_regex "$token" "$selection")"
+		result="$(perform_selection "$token" "$selection")"
 		echo "$result"
 	done
 	
 }
 
+#TODO: move such helper functions to a seperate package for security purposes
+
+function perform_selection {
+	# echo "Entered perform_selection"
+
+	local token="$1"
+	# local selection=($2)
+
+	IFS=" " read -r -a selection <<< "$2"
+
+	# echo "Selection: ${selection[0]}"
+
+	# echo "$token $selection"
+
+	if [[ ${#selection[0]} == 1 ]]
+	then
+		perform_partition_selection "$token" "${selection[@]}"
+	else
+		perform_regex_selection "$token" "${selection[@]}"
+	fi
+}
+
+function perform_regex_selection {
+	# echo "Entered perform_regex_selection"
+
+	local token="$1"
+	local selection="$2"
+
+	# echo "$token $selection"
+
+	# echo "$token" "$selection"
+	# local result="$(echo "$token" | grep -oE "$selection")"
+	local result="$(substring_regex "$token" "$selection")"
+
+	# echo "$result"
+	echo "$result"
+}
+
+#TODO: Check this implementation
+function perform_partition_selection {
+	# echo "Entered perform_partition_selection"
+
+	local token="$1"
+	# IFS=" " read -r -a selection <<< "$2"
+
+	local delim="${selection[0]}"
+	local grp_extrct="${selection[1]}"
+
+	# echo "Delimiter: $delim"
+	# echo "Group Extraction: $grp_extrct"
+
+	# if ! [[ "$grp_extrct" =~ (,?[0-9]+-*[0-9]*)+ ]]
+	# then
+	# 	echo_styled "ERROR" "$FUNCNAME" "Make Sure to give the group extraction as numbers"
+	# 	exit 1
+	# fi
+
+	# IFS=":" read -r start end <<< "${selection[1]}"
+
+	# if ! [[ ${start}${end} =~ [0-9]+ ]]
+	# then
+	# 	echo_styled "ERROR" "$FUNCNAME" "Make Sure to give the start and end index as numbers"
+	# 	exit 1
+	# fi
+
+	# if [[ $start -lt 0 || $end -lt 0 ]]
+	# then
+	# 	echo_styled "ERROR" "$FUNCNAME" "Make Sure to give the start and end index as positive numbers"
+	# 	exit 1
+	# fi
+
+	echo "$token" | cut -d "$delim" -f "$grp_extrct"
+}
+
 # result=("hello world" "hello World")
-# "$@" result[@]
+# "$@" "result[@]"
--- commands/validators/select_validator.sh
+++ commands/validators/select_validator.sh
@@ -9,17 +9,40 @@ function select_validate {
     then
         exit 1
     fi
+    
+    IFS=" " read -r -a args <<< "$@"
 
-    local selection="$1"
-    local inp_stream_ref="$2"
+    # echo ${args[@]}
 
+    local selection="${args[*]:1}"
+    local inp_stream_ref="$1"
+
+    echo "Selection: $selection"
+    echo "Input stream: $inp_stream_ref"
+
+    #TODO check these validations
     if ! declare -p "$(echo "$inp_stream_ref" | cut -d "[" -f 1 )" &>/dev/null
     then
         echo_styled "ERROR" "$FUNCNAME" "Input stream is not set"
         exit 1
     fi
 
-    if ! [[ "$selection" =~ \(.*\) ]]
+    if [[ "${#selection}" -gt 3 ]]
+    then
+        echo_styled "ERROR" "$FUNCNAME" "Selection arguments are too long"
+        exit 1
+    fi
+
+    if [[ "${#selection}" -eq 3 ]]
+    then
+        if ! [[ "$selection" =~ .\ +(,?[0-9]+-*[0-9]*)+ ]]
+        then
+            echo_styled "ERROR" "$FUNCNAME" "Wrong selection partition argument given"
+            exit 1
+        fi
+    fi
+
+    if ! [[ "${#selection}" -eq 1 && "$selection" =~ \(.*\) ]]
     then
         echo_styled "WARN" "$FUNCNAME" "Selection regex is not in brackets. Use brackets to avoid globbing"
     fi
